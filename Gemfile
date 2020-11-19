44c2a52a852da2876b74d6ba0a2cf71d66961cf8
Abhishek Verma <absk1317@gmail.com>
2020-11-19 20:01:38 +0530

    Add simplecove gem for coverage

--- Gemfile
+++ Gemfile
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 gem 'awesome_print'
 gem 'nokogiri'
 gem 'pry'
 gem 'rake'
 gem 'rspec'
 gem 'rubocop'
+gem 'simplecov', require: false
 gem 'webmock'
--- spec/stub_helper.rb
+++ spec/stub_helper.rb
@@ -7,14 +7,18 @@ RSpec.configure do |config|
   config.before(:each) do
     mock_file = File.read("#{__dir__}/fixtures/test.html")
 
-      stub_request(:get, /test.com/)
-        .with(
-          headers: {
-            'Accept' => '*/*',
-            'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-            'User-Agent' => 'Ruby'
-          }
-        )
-        .to_return(status: 200, body: mock_file, headers: {})
+    stub_request(:get, /test.com/)
+      .with(
+        headers: {
+          'Accept' => '*/*',
+          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent' => 'Ruby'
+        }
+      )
+      .to_return(status: 200, body: mock_file, headers: {})
   end
 end
+
+require 'simplecov'
+
+SimpleCov.start
